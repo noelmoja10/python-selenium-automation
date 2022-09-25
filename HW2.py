38b868a0ab65f84486b2378a44e311b3843e49cc
Noel Joseph Canlas <noelcanlas@MacBook-Air-2.hsd1.ca.comcast.net>
2022-09-25 15:52:51 -0700

    Update

---
+++ HW2.py
@@ -0,0 +1,12 @@
+'''
+Amazon Logo, driver.find_element(By.XPATH, "//*[@class='a-icon a-icon-logo']")
+Continue Button, driver.find_element(By.ID, 'continue')
+Need Help Link, driver.find_element(By.XPATH, "//*[@class='a-expander-prompt']"
+Forgot your password link, driver.find_element(By.ID, 'auth-fpp-link-bottom')
+Other issues with Sign-In link, driver.find_element(By.XPATH, "//*[@class='ap-other-signin-issues-link']")
+Create your Amazon account button, driver.find_element(By.ID, 'createAccountSubmit')
+Condition of Use, driver.find_element(By.XPATH, "//*[@href='/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088']")
+Privacy Notice Link, driver.find_element(By.XPATH, "//*[@href='/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496']")
+
+
+'''
